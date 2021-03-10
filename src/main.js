2aabef0c187d3d1a6472f09b95630e55365685fb
John C. Lavelle <jlavelle604@gmail.com>
2021-03-09 20:33:30 -0500

    Remove console.log calls

--- src/main.js
+++ src/main.js
@@ -3,14 +3,12 @@ import { display } from "./display.js"
 import { initialize, redraw } from "./view.js"
 
 const main = () => {
-  console.log("here")
   let state = initialState()
   const send = action => {
-    console.log(action)
     state = reducer(action)(state)
     redraw(document)(display(state))
   }
   initialize(document)(send)
 }
-console.log("her")
+
 main()

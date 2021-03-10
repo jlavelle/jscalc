945cb687fdc75e82c8fed318c24b410196d68076
John C. Lavelle <jlavelle604@gmail.com>
2021-03-09 19:56:04 -0500

    Implement the main function

--- src/main.js
+++ src/main.js
@@ -1,5 +1,16 @@
-const main = () => {
-  console.log("main")
-}
+import { initialState, reducer } from "./state.js"
+import { display } from "./display.js"
+import { initialize, redraw } from "./view.js"
 
+const main = () => {
+  console.log("here")
+  let state = initialState()
+  const send = action => {
+    console.log(action)
+    state = reducer(action)(state)
+    redraw(document)(display(state))
+  }
+  initialize(document)(send)
+}
+console.log("her")
 main()
